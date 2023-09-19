9225a2fe7ad68753e45170b5b2beb0e08bae4d53
Raphael Rodrigues <noh4nsen@gmail.com>
2023-09-19 15:15:33 -0300

    test: parsing and doing some tests

--- main.go
+++ main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"golang.org/x/exp/slices"
+)
 
 func main() {
-	fmt.Println("Snowchecker")
+	config := parse("./mock/test.tfvars")
+
+	var wids []string
+
+	for key, value := range config.Team {
+		if key == "PENG" {
+			fmt.Println("PENG: ", value)
+		}
+		for _, v := range value {
+			if !slices.Contains(wids, v) {
+				wids = append(wids, v)
+			}
+		}
+	}
+
+	fmt.Println()
+	fmt.Println("Total users: ", len(wids))
+	fmt.Println()
+
+	for key, value := range config.DataSource {
+		if key != value.DataSourceDomain+"__"+value.DataSourceName {
+			fmt.Println("- MART: ", key)
+			fmt.Println("-- VALUE: ", value.DataSourceDomain+"__"+value.DataSourceName)
+			fmt.Println(false)
+		}
+	}
+	fmt.Println()
+
+	for key, value := range config.DataMart {
+		if key != value.DataMartDomain+"__"+value.DataMartName {
+			fmt.Println("- MART: ", key)
+			fmt.Println("-- VALUE: ", value.DataMartDomain+"__"+value.DataMartName)
+			fmt.Println(false)
+		}
+	}
+	fmt.Println()
+
+	output, err := json.MarshalIndent(config, "", "\t")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(output))
 }
