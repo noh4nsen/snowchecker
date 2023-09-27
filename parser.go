e3eae8edbfd769e2b6864a5eb80db0b1ca0c6bf7
Raphael Rodrigues <noh4nsen@gmail.com>
2023-09-27 18:27:26 -0300

    feat: refactor parser logging

--- parser.go
+++ parser.go
@@ -1,37 +1,41 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"os"
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/gohcl"
 	"github.com/hashicorp/hcl/v2/hclparse"
 )
 
-func parse(filePath string) Config {
-	var config Config
-
-	decode(parseFile(filePath), &config)
-
-	return config
+func parse(filePaths []string) []Config {
+	var configs []Config
+	for index, filePath := range filePaths {
+		if index != 0 {
+			var config Config
+			config.filepath = filePath
+			decode(parseFile(filePath), &config)
+			configs = append(configs, config)
+		}
+	}
+	return configs
 }
 
 func parseFile(filePath string) hcl.Body {
 	parser := hclparse.NewParser()
-
 	file, diagnostics := parser.ParseHCLFile(filePath)
-
 	if diagnostics.HasErrors() {
-		log.Fatal(diagnostics.Error())
+		fmt.Println("ERROR: ", diagnostics.Error())
+		os.Exit(1)
 	}
-
 	return file.Body
 }
 
 func decode(body hcl.Body, config *Config) {
 	diagnostics := gohcl.DecodeBody(body, nil, config)
-
 	if diagnostics.HasErrors() {
-		log.Fatal(diagnostics.Error())
+		fmt.Println("ERROR: ", diagnostics.Error())
+		os.Exit(1)
 	}
 }
